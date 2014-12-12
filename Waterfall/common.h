f2c12e59c24b80c2987cb8ba307cd368ae7cb91a
novokrest <novokrest013@gmail.com>
2014-12-13 03:07:28 +0400

    add cmake support

---
+++ Waterfall/common.h
@@ -0,0 +1,30 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <cstddef>
+
+#include <vector>
+using std::vector;
+
+#include <string>
+using std::string;
+
+#include <iostream>
+using std::cout; using std::endl;
+#include <fstream>
+using std::ifstream;
+
+#include <chrono>
+namespace chrono = std::chrono;
+
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <AntTweakBar.h>
+
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+using namespace glm;
+
+#include <FreeImage.h>
+
+#endif //COMMON_H
